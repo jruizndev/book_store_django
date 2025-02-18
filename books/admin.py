31a8309ac71cff80951804a180e29f2b2a1c4193
jruizndev <jorsqn@gmail.com>
2025-02-18 19:29:56 +0100

    first commit

---
+++ books/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Book
+
+# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish_date', 'isbn')
+    search_fields = ('title', 'author', 'isbn')
+    list_filter = ('publish_date', 'author')
