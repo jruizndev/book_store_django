4096e819ccb13f37b8ec992bf16c387c68a24b46
jruizndev <jorsqn@gmail.com>
2025-02-24 13:10:20 +0100

    second day master class

--- books/views.py
+++ books/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 
 # Create your views here.
 def getAllBooks(request):
     books = Book.objects.all()
     return render(request, 'books/books.html', {'libros': books})
+
+def createBook(request):
+    if request.method == 'POST':
+        form = formBook(request.POST)
+        if form.isvalid():
+            form.save()
+            return redirect('books')
+    else: 
+        form = formBook()
+    return render(request, 'books/createBook.html', {'form': form})
+
+def updateBook(request, id):
+    book = get_object_or_404(Book, pk=id)
+    if request.method == 'PUT':
+        form = formBook(request.PUT, instance=book)
+        if form.isvalid():
+            form.save()
+            return redirect('books')
+    else: 
+        form = formBook(instance=book)
+    return render(request, 'books/updateBook.html', {'form': form, 'libro':book})
